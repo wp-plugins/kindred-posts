d3fbadb7887faf18fcd1cf471238a433a8f199e2
AiSpork <AiSpork@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-12 14:34:30 +0000

    Updates to tests.
    git-svn-id: https://plugins.svn.wordpress.org/kindred-posts/tags/1.2.4@947503 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/recommendedpost.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Start kp_recommendedPost class
+ **/
+class kp_recommendedPost {
+	public $post = null; // The WP_Post object
+	
+	public function __construct($post = null) {
+		// Check if they passed in a post id, if so, get the post
+		if (is_int($post)) {
+			$this->post = get_post($post);
+		} else {
+			$this->post = $post;
+		}
+	}
+	
+	/**
+	 * Return this object's data in an array to be used in the kp_renderer
+	 * 
+	 * @param Array $data: The data to include
+	 * @return array
+	 **/
+	public function getPostTemplateData($data = array()) {
+		$data["author_user_nicename"] = strtoupper(get_the_author_meta('user_nicename', $this->post->post_author));
+		$data["author_user_url"] = get_author_posts_url($this->post->post_author); //get_the_author_meta('user_url', $this->post->post_author);
+		$data["post_date"] = strtotime($this->post->post_date);
+		$data["post_date_nice"] = strtoupper(date("F j, Y", strtotime($this->post->post_date)));
+		$data["post_url"] = get_permalink($this->post->ID);
+		$data["post_excerpt"] = $this->post->post_excerpt;
+		$data["post_title"] = $this->post->post_title;
+		$data["has_thumbnail"] = (int)has_post_thumbnail($this->post->ID);
+		$data["post_thumbnail"] = get_the_post_thumbnail( $this->post->ID, array(150, 150) );
+		
+		$data["ga_posttitle"] = kp_prepareGoogleAnalytics($this->post->post_title);
+		$data["ga_author"] = kp_prepareGoogleAnalytics($data["author_user_nicename"]);
+	
+		// Start the widget optons
+		$data["show_post_thumbnail"] = (int)($data["kp_widget:featureimage"] == 1 && $data["has_thumbnail"]);
+		$data["show_post_title"] = (int)($data["kp_widget:posttitle"] == 1);
+		$data["show_post_author"] = (int)($data["kp_widget:postauthor"] == 1);
+		$data["show_post_date"] = (int)($data["kp_widget:postdate"] == 1);
+		$data["show_post_teaser"] = (int)($data["kp_widget:postteaser"] == 1);
+		$data["has_content"] = $data["show_post_title"] || $data["show_post_thumbnail"] || $data["show_post_author"] || $data["show_post_date"] || $data["show_post_teaser"];
+	
+		return $data;
+	}
+	
+	/**
+	 * Render the post object
+	 *
+	 * @param string $template: The template to use when rendering the object
+	 * @param array $data: Additional data we want to pass into the template
+	 * @return string: The rendered Html
+	 **/
+	public function render($template = "", $data = array()) {
+		global $kp_templates;
+		if ($template == "" && isset($kp_templates["kp_recommendedPost"])){
+			$template = $kp_templates["kp_recommendedPost"];
+		}
+		
+		$data = $this->getPostTemplateData($data);
+		return kp_renderer::render($template, $data);
+	}
+} // End kp_recommendedPost class
+?>
---
+++ tests/tests-loader.php
@@ -0,0 +1,58 @@
+<?php
+// Load configuration, functions, and classes for the plugin
+include_once("..\..\..\..\wp-load.php");
+include_once("..\kindred-posts-index.php");
+
+// Load the test classes
+include_once('classes\recommendedpost.php');
+include_once('classes\recommender.php');
+include_once('classes\renderer.php');
+include_once('classes\test.php');
+include_once('classes\widget.php');
+
+$testPostID = 1; // A sample post to use for testing
+$numTestsToInsert = -1; // Set to <= 0 if you want to insert data
+$testRenderer = false || (isset($_GET["testAll"]) && ($_GET["testAll"] == "true"));
+$testRecommendedPost = false || (isset($_GET["testAll"]) && ($_GET["testAll"] == "true"));
+$testRecommender = false || (isset($_GET["testAll"]) && ($_GET["testAll"] == "true"));
+$testWidget = false || (isset($_GET["testAll"]) && ($_GET["testAll"] == "true"));
+
+$testRenderer = $testRenderer || (isset($_GET["testRenderer"]) && ($_GET["testRenderer"] == "true"));
+$testRecommendedPost = $testRecommendedPost || (isset($_GET["testRecommendedPost"]) && ($_GET["testRecommendedPost"] == "true"));
+$testRecommender = $testRecommender || (isset($_GET["testRecommender"]) && ($_GET["testRecommender"] == "true"));
+$testWidget = $testWidget || (isset($_GET["testWidget"]) && ($_GET["testWidget"] == "true"));
+
+if ($numTestsToInsert > 0) {
+	echo "<div>Inserting $numTestsToInsert Test Post(s)...</div>";
+	$testDataObj = new kp_testData();
+	$testDataObj->insertTestPosts($numTestsToInsert);
+}
+
+if ($testRenderer) {
+	// Test the renderer class
+	echo "<div>Testing classes\\renderer</div>";
+	$renderTestObj = new kp_test_renderer();
+	$renderTestObj->runTests();
+}
+
+if ($testRecommendedPost) {	
+	// Test the recommendedPost class
+	echo "<div>Testing classes\\recommendedPost</div>";
+	$recommendedPostTestObj = new kp_test_recommendedPost($testPostID);
+	$recommendedPostTestObj->runTests();
+}
+
+if ($testRecommender) {
+	// Test the recommender class
+	echo "<div>Testing classes\\recommender</div>";
+	$recommenderTestObj = new kp_test_recommender();
+	$recommenderTestObj->runTests();
+}
+
+if ($testWidget) {
+	// Test the widget class
+	echo "<div>Testing classes\\widget</div>";
+	$recommenderTestObj = new kp_test_widget();
+	$recommenderTestObj->runTests();
+}
+?>
